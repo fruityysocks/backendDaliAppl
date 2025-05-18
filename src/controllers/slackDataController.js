a9d4e277257ecdc6a67ffe79205202db1db0b700
fruityysocks <prishita.t.dharampal.28@dartmouth.edu>
2025-05-18 15:21:56 -0400

    testing

--- src/controllers/slackDataController.js
+++ src/controllers/slackDataController.js
@@ -1,48 +1,48 @@
-const fs = require('fs');
-const path = require('path');
-const { getUser } = require('./userController');
+// const fs = require('fs');
+// const path = require('path');
+// const { getUser } = require('./userController');
 
-const readAllFiles = (folderPath) => {
-  const files = fs.readdirSync(folderPath);
-  let allData = [];
+// const readAllFiles = (folderPath) => {
+//   const files = fs.readdirSync(folderPath);
+//   let allData = [];
 
-  files.forEach((file) => {
-    if (file.endsWith('.json')) {
-      const filePath = path.join(folderPath, file);
-      const data = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
-      allData = allData.concat(data);
-    }
-  });
+//   files.forEach((file) => {
+//     if (file.endsWith('.json')) {
+//       const filePath = path.join(folderPath, file);
+//       const data = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+//       allData = allData.concat(data);
+//     }
+//   });
 
-  return allData;
-};
+//   return allData;
+// };
 
-const usersPath = path.join(__dirname, '..', 'users.json');
+// const usersPath = path.join(__dirname, '..', 'users.json');
 
-const users = JSON.parse(fs.readFileSync(usersPath, 'utf-8'));
+// const users = JSON.parse(fs.readFileSync(usersPath, 'utf-8'));
 
-const getSlackUser = (userId) => {
-  const user = getUser(userId);
-  const slackuser = users.find((u) => { return u.name === user.name; });
-  return slackuser;
-};
+// const getSlackUser = (userId) => {
+//   const user = getUser(userId);
+//   const slackuser = users.find((u) => { return u.name === user.name; });
+//   return slackuser;
+// };
 
-const napDataPath = path.join(__dirname, '../slackData/daliNaps');
-const napData = readAllFiles(napDataPath);
+// const napDataPath = path.join(__dirname, '../slackData/daliNaps');
+// const napData = readAllFiles(napDataPath);
 
-const getNaps = (req, res) => {
-  const allNaps = napData.filter((nap) => { return nap.subtype !== 'channel_join' && nap.subtype !== 'channel_leave'; });
-  console.log(allNaps);
-  res.json(allNaps);
-};
+// const getNaps = (req, res) => {
+//   const allNaps = napData.filter((nap) => { return nap.subtype !== 'channel_join' && nap.subtype !== 'channel_leave'; });
+//   console.log(allNaps);
+//   res.json(allNaps);
+// };
 
-const getNap = (singleNap) => {
-  return {
-    message: singleNap.text,
-    user: singleNap.user,
-    img: singleNap.thumb_480,
-  };
-};
+// const getNap = (singleNap) => {
+//   return {
+//     message: singleNap.text,
+//     user: singleNap.user,
+//     img: singleNap.thumb_480,
+//   };
+// };
 
 // const getTopNapper = (req, res) => {
 //   const counts = {};
@@ -56,4 +56,4 @@ const getNap = (singleNap) => {
 //   res.json({ topNapper: topUser, naps: count });
 // };
 
-export { getSlackUser, getNaps, getNap };
+// export { getSlackUser, getNaps, getNap };
