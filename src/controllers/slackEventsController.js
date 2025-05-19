2303107cc2126ca20d8cc01f877670716dce0b86
fruityysocks <prishita.t.dharampal.28@dartmouth.edu>
2025-05-19 11:51:04 -0400

    testing

--- src/controllers/slackEventsController.js
+++ src/controllers/slackEventsController.js
@@ -145,8 +145,8 @@ export async function generatePoemFromImage(imageUrl) {
           role: 'user',
           content: [
             {
-              type: 'image_file',
-              image_file: { file_id: file.id },
+              type: 'file',
+              file: { file_id: file.id },
             },
           ],
         },
