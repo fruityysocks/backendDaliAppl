1ffc8678200b77fd1b16dbe29b5581c61726c34f
fruityysocks <prishita.t.dharampal.28@dartmouth.edu>
2025-05-19 17:10:55 -0400

    testing

--- src/server.js
+++ src/server.js
@@ -31,7 +31,7 @@ app.get('/import-old-naps', async (req, res) => {
   try {
     const { assisstantId, threadId } = await initialisingOpenAI();
     await fetchOldNaps(napChannelId, assisstantId, threadId);
-    console.log(assisstantId, threadId);
+    console.log('assistant id', assisstantId, threadId);
     res.setHeader('Content-Type', 'text/plain');
     res.send('Imported old naps!');
   } catch (err) {
