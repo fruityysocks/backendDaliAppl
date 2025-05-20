04b14fbd4d738daa7a135c2a47a3e5c2b29f75c3
fruityysocks <prishita.t.dharampal.28@dartmouth.edu>
2025-05-20 13:09:37 -0400

    testing

--- src/controllers/openAIController.js
+++ src/controllers/openAIController.js
@@ -11,15 +11,15 @@ const openai = new OpenAI({
 
 export async function initialisingOpenAI() {
   const assistant = await createAssistant();
-  const thread = await openai.beta.threads.create();
+  // const thread = await openai.beta.threads.create();
   const assistantId = assistant.id;
-  const threadId = thread.id;
-  await openai.beta.threads.messages.create(threadId, {
-    role: 'user',
-    content: 'Write a three sentence long poem for each of the images below. Keep it short and funny; do not make potentially offensive jokes or use curse words.',
-  });
+  // const threadId = thread.id;
+  // await openai.beta.threads.messages.create(threadId, {
+  //   role: 'user',
+  //   content: 'Write a three sentence long poem for each of the images below. Keep it short and funny; do not make potentially offensive jokes or use curse words.',
+  // });
 
-  return { assistantId, threadId };
+  return { assistantId };
 }
 
 async function createAssistant() {
@@ -39,7 +39,7 @@ async function createAssistant() {
   }
 }
 
-export async function generatePoemFromImage(imageUrl, assisstantId, threadId) {
+export async function generatePoemFromImage(imageUrl, assisstantId) {
   try {
     if (!imageUrl) {
       throw new Error('Image URL is required to generate poem.');
@@ -58,6 +58,13 @@ export async function generatePoemFromImage(imageUrl, assisstantId, threadId) {
     const file = await openai.files.create({
       file: fs.createReadStream(tempPath),
       purpose: 'vision',
+    });
+    const thread = await openai.beta.threads.create();
+    const threadId = thread.id;
+
+    await openai.beta.threads.messages.create(threadId, {
+      role: 'user',
+      content: 'Write a three sentence long poem for each of the images below. Keep it short and funny; do not make potentially offensive jokes or use curse words.',
     });
 
     await openai.beta.threads.messages.create(threadId, {
