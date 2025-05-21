3e3804f4ca0136871f79fe3b6a87052707c25a38
fruityysocks <prishita.t.dharampal.28@dartmouth.edu>
2025-05-20 23:22:50 -0400

    testing

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,4 @@
 import { Router } from 'express';
-import * as Posts from './controllers/postController';
 import * as Users from './controllers/userController';
 import * as Slacks from './controllers/slackEventsController';
 import User from './models/userModel';
@@ -73,76 +72,6 @@ router.route('/users/:userId')
       console.error('error deleting user:', error);
       return res.status(500).json({ error: `delete user error: ${error}` });
     }
-  });
-
-router.route('/users/:userId/allPosts')
-  .get(async (req, res) => {
-    try {
-      const posts = await Users.getPostsByUser(req.params.userId);
-      if (posts.length === 0) {
-        return res.status(404).json({ error: 'no posts found for this user' });
-      }
-      return res.json(posts);
-    } catch (error) {
-      console.error('error finding posts for user:', error);
-      return res.status(404).json({ error: `get posts for user error: ${error}` });
-    }
-  });
-
-// post routes
-
-router.route('/users/:userId/:postId')
-  .get(async (req, res) => {
-    try {
-      const post = await Posts.getPost(req.params.postId);
-      if (!post) {
-        return res.status(404).json({ error: 'post not found' });
-      }
-      return res.json(post);
-    } catch (error) {
-      console.error('error getting post:', error);
-      return res.status(404).json({ error: `get post error: ${error}` });
-    }
-  })
-
-  .put(async (req, res) => {
-    try {
-      const post = await Posts.updatePost(req.params.postId, req.body);
-      if (!post) {
-        return res.status(404).json({ error: 'post not found' });
-      }
-      return res.json(post);
-    } catch (error) {
-      console.error('error getting post:', error);
-      return res.status(500).json({ error: `get post error: ${error}` });
-    }
-  })
-
-  .delete(async (req, res) => {
-    try {
-      await Posts.deletePost(req.params.postId);
-      return res.json({ message: 'post deleted successfully' });
-    } catch (error) {
-      console.error('error deleting post:', error);
-      return res.status(500).json({ error: `delete post error: ${error}` });
-    }
-  });
-
-router.route('/posts')
-  .get(async (req, res) => {
-    try {
-      const posts = await Posts.getPosts();
-      res.json(posts);
-    } catch (error) {
-      console.error('error getting posts:', error);
-      res.status(404).json({ error: `get posts error: ${error}` });
-    }
-  });
-
-router.route('/users/:userId/newPost')
-  .post(async (req, res) => {
-    const post = await Posts.createPost(req.body);
-    res.status(200).json(post);
   });
 
 router.route('/naps')
--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const app = express();
 dotenv.config();
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://frontenddaliappl.onrender.com',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
